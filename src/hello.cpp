95f6ef406d215d36fa05c460c18ef99ff9393cad
Yaric Shkurat <yarrik@Yarics-MacBook-Pro.local>
2020-06-04 16:06:39 +0300

    Initial commit

---
+++ src/hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "BTLightConfig.h"
+
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        // report version
+        std::cout << argv[0] << " Version "
+                  << BTLight_VERSION_MAJOR << "."
+                  << BTLight_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " name" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Hello " << argv[1] << "!!!" << std::endl;
+    return 0;
+}
