db41097f19624cc273dc9aadf699569930acf52f
Yaric Shkurat <yarrik@Yarics-MacBook-Pro.local>
2020-06-04 16:08:03 +0300

    Improved help message

--- src/hello.cpp
+++ src/hello.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
     if (argc < 2)
     {
         // report version
-        std::cout << argv[0] << " Version "
+        std::cout << argv[0] << "Hello Sayer Version "
                   << BTLight_VERSION_MAJOR << "."
                   << BTLight_VERSION_MINOR << std::endl;
         std::cout << "Usage: " << argv[0] << " name" << std::endl;
