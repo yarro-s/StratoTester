d947cb7dcda7095c47a67a834f108babce0634d2
Yaric Shkurat <yarrik@Yarics-MacBook-Pro.local>
2020-07-01 12:53:26 +0300

    Templates added

--- src/main.cxx
+++ src/main.cxx
@@ -17,18 +17,10 @@ int main()
     bt::price_t pT {
         20.5, 10.5, 12.3, 9.0, 10.4, 10.5, 14.5, 14.5, 46.5};
     bt::weight_t wT {
-        0.7,  0.5, 0.5,  0.9,  0.9,  0.3,  0.0,  0.0, 0.0};
+         0.7,  0.5,  0.5, 0.9,  0.9,  0.3,  0.0,  0.0,  0.0};
 
-    // auto base_strat = bt::weight_alloc(wT);
-    auto strat = bt::strategy(new bt::weight_alloc(wT)).lookback(3)
-        .rebalance_every(2);
-
-    // bt::weight_alloc strat0(wT);
-    // bt::asset_alloc_lb strat1(strat0, 3);
-    // bt::asset_alloc_rb strat(strat1, 4);
-
-    // bt::strategy strat0 = bt::strategy(bt::weight_alloc(wT));
-    // bt::strategy strat = bt::strategy(bt::asset_alloc_lb(strat0, 3));
+    auto strat = bt::strategy<bt::weight_alloc>(wT)
+        .lookback(3).rebalance_every(2);
 
     bt::single_asset back_test(strat);
     auto const &res = back_test.run(pT);
