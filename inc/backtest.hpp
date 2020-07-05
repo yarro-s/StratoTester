9ff712aabc36a5d1324ee3ee12cd8e8a1372dbef
Yaric Shkurat <yarrik@Yarics-MacBook-Pro.local>
2020-07-05 21:36:25 +0300

    Fixed formatting

--- inc/backtest.hpp
+++ inc/backtest.hpp
@@ -1,16 +1,14 @@
+// Copyright 2020 Yarro S
+
 #pragma once
 
 #include <unit_defs.hpp>
 
 
-namespace bt
-{
-    class backtest
-    {
-
-    public:
-        virtual backtest &update(price px, weight w) = 0;
-        virtual backtest &run(price_t const &pT) = 0;
-    };
-}
-
+namespace bt {
+class backtest {
+ public:
+    virtual backtest &update(price px, weight w) = 0;
+    virtual backtest &run(price_t const &pT) = 0;
+};
+}  // namespace bt
