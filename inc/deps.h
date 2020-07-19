ebedef667a3d6da0b3916e7e8d392c034bda1fe1
Yaric Shkurat <yarrik@Yarics-MacBook-Pro.local>
2020-07-19 09:30:26 +0300

    Removed extra dependencies

--- inc/deps.h
+++ inc/deps.h
@@ -1,39 +1,15 @@
+// Copyright 2020 Yarro S
+
 #pragma once
 
-#include <vector>
-#include <numeric>
 #include <iostream>
-
-#include <vector>
-#include <iostream>
-
-#include <iostream>
-#include <functional>
-
-#include <cmath>
-#include <iostream>
-
-#include <cmath>
-
-#include <vector>
+#include <memory>
 #include <set>
 #include <string>
 #include <unordered_map>
-#include <ctime>
-#include <sstream>
-#include <iostream>
-
 #include <vector>
+
 #include <chrono>
-#include <string>
 
-#include <string>
-
-#include <limits>
-#include <algorithm>
-#include <numeric>
-#include <iostream>
-
-#include <cmath>
-#include <iostream>
+#include <functional>
 
