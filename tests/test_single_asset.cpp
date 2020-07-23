b656d3142668c7a6c839810e11aa9dd23b3dd268
Yaric Shkurat <yarrik@Yarics-MacBook-Pro.local>
2020-07-23 19:18:52 +0300

    Added arbitrary weighting test

--- tests/test_single_asset.cpp
+++ tests/test_single_asset.cpp
@@ -19,6 +19,26 @@
 TEST_CASE("Changing portfolio", "[single_asset][result]") {
     bt::price const init_deposit = 10000;
 
+    SECTION("Arbitrary weights") {
+        // bt::price const init_cash = 2000;
+        // bt::price_t pT{100,   125,    92,    50,   200,  165};
+        // bt::weight_t wT{    0.313, 0.351, 0.130, 0.747, 0.887};
+
+        bt::price const init_cash = 10000;
+        bt::price_t pT{000, 75.2, 125.3, 36.8,  305,  145,  8.5, 45.3};
+        bt::weight_t wT{    0.24,  0.26, 0.52, 0.73, 0.61, 0.44,  000};
+
+        bt::weight_alloc strat(wT);
+        bt::single_asset back_test(strat, init_cash);
+
+        auto res = back_test.run(pT).results();
+
+        auto const pv_expected =
+            "[10000, 11553.1, 9517.6, 45456.4, 28176.4, 12069.4, 35032.6]";
+
+        REQUIRE(pv_expected == bt::str_rep(res.pv()));
+    }
+
     SECTION("Changing weight and asset price") {
         bt::price_t pT{ 150, 180, 250};
         bt::weight_t wT{0.7, 0.8, 0.5};
@@ -34,7 +54,7 @@ TEST_CASE("Changing portfolio", "[single_asset][result]") {
         auto const end_val_expected =
             n_asset_expected * pT.back() + cash_expected;
 
-        CHECK(Approx(end_val_expected / init_deposit)
+        REQUIRE(Approx(end_val_expected / init_deposit)
                 == res.growth());
     }
 }
